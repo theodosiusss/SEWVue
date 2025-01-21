c87ca44aa07e6f0a2296156ddfd782cbcbb1fe20
Theodosiusss <1202@htl.rennweg.at>
2025-01-21 08:32:44 +0100

    slot angepasst

--- src/AdderButton.vue
+++ src/AdderButton.vue
@@ -1,42 +1,14 @@
 <template>
-  <button name="hallo" @click="onClick">
+  <button >
     <slot>Default Button Text</slot>
   </button>
 </template>
 
 <script>
-export default {
-  name: "AdderButton",
-  props: {
-    onClickHandler: {
-      type: Function,
-      default: null,
-    },
-  },
-  methods: {
-    onClick() {
-      if (this.onClickHandler) {
-        this.onClickHandler();
-      } else {
-        this.$emit("click");
-      }
-    },
-  },
-};
+
+
 </script>
 
 <style scoped>
-/* Hier können Sie die Stile für den Button anpassen */
-button {
-  background-color: #007bff;
-  color: white;
-  border: none;
-  padding: 10px 20px;
-  border-radius: 5px;
-  cursor: pointer;
-}
 
-button:hover {
-  background-color: #0056b3;
-}
 </style>
